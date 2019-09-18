ab4c1e19895b973bb39a7938ff2bfc496a18dde6
fumischo <nil.admirari1009@gmail.com>
2019-09-18 14:11:06 +0900

    web課題

---
+++ test.rb
@@ -0,0 +1,29 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+# 課題
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'assignment.html.erb')
+
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+# この一行を追記
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+
+server.start
+
